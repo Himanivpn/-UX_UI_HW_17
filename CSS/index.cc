d0cd9fbda068e1b69d86bd1004d218a0b1289177
Himanivpn <himanikulkarnisingh@yahoo.com>
2023-02-01 11:29:00 -0500

    changes in code
    change in code and added additional images

---
+++ CSS/index.cc
@@ -0,0 +1,60 @@
+@charset "utf-8";
+/* CSS Document */
+/* Main Div css */
+.container {
+    width: 1219px;
+    height: 1663px;
+    background-image: url("../Images/backgroundimg.png");
+    background-position: center;
+    background-repeat: no-repeat;
+        
+
+}
+
+.h2 {
+    font-family: ;
+
+}
+
+}
+.header {
+    width: 600px;
+    height: 200px;
+    font-family: cursive;
+    float: right;   
+   
+   
+
+ 
+    
+    
+}
+.logo{
+    font-family: sans-serif,Kadwa;
+    font-size: 20px;
+}
+.nav
+{
+    font: FingerPaint;
+    font-size: 20px;
+    color: darkblue;
+    padding: 30px;
+}
+.body {
+width: 1219px;
+height:1000px;
+font-family: Finger Paint;
+font-size: 20px;
+
+}
+.footer
+{
+   
+   background-color: black;
+    opacity: 0.5;
+    height:20px;
+    width:0 auto;
+   
+    
+  
+}
